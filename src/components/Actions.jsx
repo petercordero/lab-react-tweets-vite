4b30e5d916bd9d7f201a5b8dc8df4130aca14477
Peter Cordero <petercordero29@gmail.com>
2023-07-25 10:51:04 -0400

    lab done

---
+++ src/components/Actions.jsx
@@ -0,0 +1,10 @@
+const Actions = ({actions}) => {
+  return (
+    <div className="actions">
+          {/* Font Awesome icons */}
+          {actions}
+        </div>
+  );
+}
+
+export default Actions
